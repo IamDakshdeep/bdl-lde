cd72e816b35324fab986dd2b5759a77bac7aca65
Dakshdeep Singh <77427849+IamDakshdeep@users.noreply.github.com>
2023-02-08 15:30:53 -0500

    Update RTM_V2.R

--- RTM_V2.R
+++ RTM_V2.R
@@ -1,3 +1,7 @@
+library(readxl)
+library(tidyr)
+library(dplyr)
+
 destfile <- "Data/Global_Merchandise_Trade.xlsx"
 
 url = "https://www.cpb.nl/sites/default/files/omnidownload/CPB-World-Trade-Monitor-April-2022.xlsx"
